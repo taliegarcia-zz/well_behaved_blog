e7c124c0e0727e158af03557d84f73556057ea06
taliegarcia <taliegarcia@gmail.com>
2016-02-14 14:53:45 -0800

    user-signin; setup feature, tests passing

---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "User sign in" do
+
+  before do
+    @grace = User.create(email: "grace@example.com", password: "password")
+  end
+
+  scenario "User signs in with valid credentials" do
+    visit "/"
+
+    click_link "Sign in"
+    fill_in "Email", with: @grace.email
+    fill_in "Password", with: @grace.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{@grace.email}")
+  end
+
+end
