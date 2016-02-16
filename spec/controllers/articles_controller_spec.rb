6a571446f72a329cd13b0f3f2ecdbdd9cbbc1a0a
taliegarcia <taliegarcia@gmail.com>
2016-02-16 14:31:21 -0800

    restrict-controller-actions; implemented tests and if statements in controller to restrict access to controller actions. tests passing. wonder if this could also be accomplished with a before_action filter instead of repeating the logic in each action.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe ArticlesController, type: :controller do
 
   describe "GET #index" do
+    before do
+      @grace = User.create(email: "grace@example.com", password: "password")
+    end
+
+    context "owner is allowed to edit her articles" do
+      it "renders the edit template" do
+        login_user @grace
+        article = Article.create(title: "First Article", body: "Body text of first test article", user: @grace)
+
+        get :edit, id: article
+        expect(response).to render_template :edit
+      end
+    end
+
+    context "non-owner is not allowed to edit other users articles" do
+      it "redirects to the root path" do
+        @tamara = User.create(email: "tamara@example.com", password: "password")
+        login_user @tamara
+        article = Article.create(title: "First Article", body: "Body text of first test article", user: @grace)
+
+        get :edit, id: article
+        expect(response).to redirect_to(root_path)
+        message = "You can only edit your own article."
+        expect(flash[:danger]).to eq message
+      end
+    end
+
+
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
